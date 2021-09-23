a10028f50b639ee0f99e649c9e70464f0c3787e6
Ian G <yo.ian.g@gmail.com>
2021-09-23 21:36:54 +0000

    deploy Thu Sep 23 2021 21:36:54 GMT+0000 (Coordinated Universal Time)

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,19 @@
 // Based on https://codepen.io/nashvail/pen/wpGgXO by Nash Vail
 class RandomShapes extends HTMLElement {
   constructor() {
-    super(); // this.attachShadow({ mode: "open" });
-
-    this.colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+    super();
+    this.colors = ["#70D6FF", "#FF70A6", "#FF9770", "#FFD670", "#E9FF70"];
     this.shapesTotal = 50;
     this.shapes = [];
     this.shapesContainer = undefined;
   }
 
   connectedCallback() {
+    const prepareColors = this.getAttribute("colors") ? this.getAttribute("colors").replaceAll("'", "\"") : null;
+    const colorsAttributes = JSON.parse(prepareColors);
+    this.colors = Array.isArray(colorsAttributes) ? colorsAttributes : this.colors;
+    this.shapesTotal = this.getAttribute("shapes-total") || this.shapesTotal;
+
     if (this.shapesContainer === undefined) {
       this.shapesContainer = document.createElement('div');
       this.appendChild(this.shapesContainer);
@@ -18,50 +22,45 @@ class RandomShapes extends HTMLElement {
     if (this.shapes.length === 0) {
       for (let count = 0; count < this.shapesTotal; count++) {
         let shape = document.createElement("div");
-        shape.size = Math.random() < 0.1 ? Math.random() * 10 : Math.random();
+        const size = Math.random() < 0.2 ? Math.random() * 10 : Math.random();
         shape.classList.add("shape");
         shape.style.position = 'absolute';
-        shape.style.opacity = Math.random() * 0.7 * shape.size;
+        shape.style.opacity = Math.random() * 0.7 * size;
         shape.style.borderRadius = `${Math.random() * 100}%`;
         shape.style.background = this.colors[Math.floor(Math.random() * this.colors.length)];
         shape.style.left = `${Math.floor(Math.random() * 100)}vw`;
         shape.style.top = `${Math.floor(Math.random() * 100)}vh`;
         shape.style.transform = `scale(${Math.random()})`;
-        shape.style.width = `${shape.size}em`;
+        shape.style.width = `${size}em`;
         shape.style.height = shape.style.width;
+        shape.style.zIndex = Math.floor(size * 100);
+        this.animateShape(shape, this.shapes.length, size);
         this.shapes.push(shape);
         this.shapesContainer.appendChild(shape);
       }
     }
-
-    this.shapes.forEach((shape, i, ra) => {
-      let additionFrameChance = 0.5;
-      const keyFrames = ["translate(0, 0)"];
-
-      while (keyFrames.length < 2 || Math.random() < additionFrameChance) {
-        keyFrames.push(`translate(${Math.random() * (i % 2 === 0 ? -11 : 11)}rem, ${Math.random() * 12}rem)`);
-        additionFrameChance *= 0.75;
-      }
-
-      let anim = shape.animate(keyFrames.map(translate => {
-        return {
-          transform: translate
-        };
-      }), {
-        duration: (Math.random() + 1) * (2000 * shape.size),
-        direction: Math.random() > 0.5 ? "alternate" : "alternate-reverse",
-        fill: "both",
-        iterations: Infinity,
-        easing: "ease-in-out"
-      });
-    }); // this.shadowRoot.appendChild(template.content.cloneNode(true));
-    // this.shadowRoot.getElementById("inc").onclick = () => this.inc();
-    // this.shadowRoot.getElementById("dec").onclick = () => this.dec();
-
-    this.update();
   }
 
-  update() {// requestAnimationFrame(this.update.bind(this))
+  animateShape(shape, index, size) {
+    let additionFrameChance = 0.5;
+    const keyFrames = ["translate(0, 0)"];
+
+    while (keyFrames.length < 2 || Math.random() < additionFrameChance) {
+      keyFrames.push(`translate(${Math.random() * (index % 2 === 0 ? -11 : 11)}rem, ${Math.random() * 12}rem)`);
+      additionFrameChance *= 0.75;
+    }
+
+    let anim = shape.animate(keyFrames.map(translate => {
+      return {
+        transform: translate
+      };
+    }), {
+      duration: (Math.random() + 1) * (2000 * size),
+      direction: Math.random() > 0.5 ? "alternate" : "alternate-reverse",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    });
   }
 
 }
